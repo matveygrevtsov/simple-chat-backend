19f6c0485870e41539382d43d0c1f8f090ae9be4
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-04-24 14:35:40 +0300

    Добавил файл README.md (#2)
    * Добавил файл README.md
    
    * Добавил файл README.md
    
    ---------
    
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import { PORT } from "./constants";
 import express from "express";
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
-
 app.listen(PORT, () =>
   console.log(`Сервер успешно стартовал на порту ${PORT}`)
 );
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!!");
+});
