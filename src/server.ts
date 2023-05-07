3a4f45d9b1a92bb15c426895bf8b6fd07c623e28
matveygrevtsov <43029770+matveygrevtsov@users.noreply.github.com>
2023-05-07 14:27:02 +0300

    Добавил cors (#14)
    Co-authored-by: matveygrevtsov <matveygrevcov@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from "express";
 import { router } from "./router/rootRouter";
+import express from "express";
+import cors from "cors";
 
 export const server = express();
 
+server.use(cors());
 server.use(express.json()); // Чтобы наше приложение могло парсить JSON-формат.
 server.use("/api", router);
 
